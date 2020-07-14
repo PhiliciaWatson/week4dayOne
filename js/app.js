2b655830ac0d49766b90daa017ec17c20911d34a
PhiliciaWatson <philiciawatson7@gmail.com>
2020-07-14 07:28:58 -0400

    resubmit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+var cold = {
+
+            'ice':'cream',
+            'love': 4,
+             'sing':'[1,2,3,4]'
+            
+            };
+
+
+            var cold2val = cold.ice;
+            console.log(cold2val);
+
+var sea = {
+    
+            'coke':2,
+            'dance':'just dance',
+            'cook':'[cake,rice,mashed potatoes]'
+        
+        };
+
+        var sea2val = sea.dance;
+        console.log(sea2val);
+
+var ceasar = {
+    
+    'pizza':7,
+    'live':'together',
+    'eat':'[breakfast,lunch,dinner]'
+
+    };
+
+    var ceasar2val = ceasar.eat;
+    console.log(ceasar2val);
+
+
+
+    let myarr = [1,2,3,4,5,6]
+
+for(let i = 0; i < myarr.length; i++){
+console.log(myarr);
+}
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
